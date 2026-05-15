9b37441915ebb81d325bcb3aebe212da7630443b
Brett Smith <brett@pukekos.co.nz>
2026-05-15 12:54:45 +1200

    chore(apphost): revert to plain addViteApp, drop WORKSPEC branching

--- apphost.ts
+++ apphost.ts
@@ -2,15 +2,8 @@ import { createBuilder } from './.modules/aspire.js';
 
 const builder = await createBuilder();
 
-if (process.env.WORKSPEC) {
-  await builder
-    .addJavaScriptApp('web', './artifacts/web', { runScriptName: 'dev:workstation' })
-    .withHttpEndpoint({ port: 8080, env: 'VITE_PORT' })
-    .withPnpm();
-} else {
-  await builder
-    .addViteApp('web', './artifacts/web')
-    .withPnpm();
-}
+await builder
+  .addViteApp('web', './artifacts/web')
+  .withPnpm();
 
 await builder.build().run();
